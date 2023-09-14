fb9111d44514829e0d55112e456ce4e9dd0161bf
lxt <2733727225@qq.com>
2023-09-14 14:53:30 +0800

    9.14 14点52分 学习更新

---
+++ android/app/src/main/java/com/example/studydart/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.studydart;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- lib/study/List2.dart
+++ lib/study/List2.dart
@@ -30,4 +30,15 @@ void main(){
   var oddNum = nums .where((element) => isOdd(element));
   print(oddNum);
   //2023 9 08
+
+print(nums.any(isOdd));//有一个奇数就返回true
+print(nums.every((element) => isOdd(element)));//判断是否都是奇数
+
+  var oo = [[1,2],[3,4]] ;
+
+  var nn = oo.expand((element)=>element).toList();
+  print(nn);
+
+  int rr = nums .fold(2, (p, element) => p*element);
+  print(rr);
 }
